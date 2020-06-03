15d928d7dad42812170f0b02699f34a9b8495f43
sagnikchoudhury18 <sagnikchoudhury18@gmail.com>
2020-06-04 01:09:51 +0530

    Added function for state and onsubmit in signup along with success message

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -8,7 +8,7 @@ const Base = ({
     children
 }) => (
     <div>
-        <Menu/>
+       <Menu/>
         <div className="container-fluid">
             <div className="jumbotron bg-dark text-white text-center">
                 <h2 className="display-4">{title}</h2>
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+        }
+      })
+      .catch(console.log("Error in signup"));
+  };
+
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+// onSubmit without () is used if function is not called immidately whereas if a func is called immidiately we use ()
+
+const successMessage = () => {
+    return(
+    <div className="alert alert-success"
+    style={{display: success ? "":"none"}}>
+        New account was created successfully. Please {""}
+        <Link to="/signin">Login Here</Link>
+    </div>
+    )
+}
+
+const errorMessage = () => {
+    return(
+    <div className="alert alert-danger"
+    style={{display: error ? "":"none"}}>
+        {error}
+    </div>
+    )
+}
+
+  return (
+    <Base title="Sign up page" description="A page for user to sign up!">
+        {successMessage()}
+        {errorMessage()}
+        {signUpForm()}
+        <p className="text-light text-center">{JSON.stringify(values)}</p>
+    </Base>
+  );
+};
+
+export default Signup;
